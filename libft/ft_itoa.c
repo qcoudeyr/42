ffa13a0c5771d9a272b4b7887e2f5477c77e87b0
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-02-13 15:47:38 +0100

    itoa 100%

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,58 +6,73 @@
 /*   By: qcoudeyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:59:48 by qcoudeyr          #+#    #+#             */
-/*   Updated: 2023/02/13 14:50:38 by qcoudeyr         ###   ########.fr       */
+/*   Updated: 2023/02/13 15:47:17 by qcoudeyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 
-static int	ft_intlen(int n, int *pwr)
+static int	ft_intlen(long *n, long *pwr)
 {
-	int	len;
+	long	len;
+	long	val;
 
-	len = 0;
+	len = 1;
 	*pwr = 1;
-	if (n < 0)
+	if (*n < 0)
 	{
 		len += 1;
-		n *= -1;
+		*n *= -1;
 	}
-	while (n != 0)
+	val = *n;
+	while (val != 0)
 	{
-		if (n > 9)
+		if (val > 9)
 			*pwr *= 10;
-		n /= 10;
+		val /= 10;
 		len++;
 	}
 	return (len);
 }
 
+static void	*ft_strindexer(char *str, long *val, int *index, long *pwr)
+{
+	while (*pwr != 1)
+	{
+		str[*index] = (*val / *pwr) + 48;
+		*val %= *pwr;
+		*pwr /= 10;
+		*index += 1;
+	}
+	str[*index] = *val +48;
+	*index += 1;
+	str[*index] = 0;
+	return (str);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
-	int		pwr;
+	long	pwr;
 	int		index;
 	long	val;
 
 	index = 0;
 	val = n;
-	str = malloc(sizeof(char) * ft_intlen(val, &pwr) + 1);
+	if (n == 0)
+		str = malloc(sizeof(char) * 2);
+	else
+		str = malloc(sizeof(char) * ft_intlen(&val, &pwr));
 	if (!str)
 		return (NULL);
-	if (val < 0)
-	{
+	if (n < 0)
 		str[index++] = '-';
-		val *= -1;
-	}
-	while (pwr != 1)
-	{
-		str[index++] = (val / pwr) + 48;
-		val %= pwr;
-		pwr /= 10;
-	}
-	str[index++] = val +48;
+	if (n == 0)
+		str[index++] = 0 +48;
+	else
+		ft_strindexer(str, &val, &index, &pwr);
 	str[index] = 0;
 	return (str);
 }
