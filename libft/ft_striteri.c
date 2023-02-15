0b26d4a8493e29a51650fc633365f4019a1ed301
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-02-15 16:08:14 +0100

    100% plus qu'a finir split et les 2-3 crash et on attaque les bonus

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,8 +6,20 @@
 /*   By: qcoudeyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:59:50 by qcoudeyr          #+#    #+#             */
-/*   Updated: 2023/02/14 11:33:26 by qcoudeyr         ###   ########.fr       */
+/*   Updated: 2023/02/15 15:58:21 by qcoudeyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		f(i, s + i);
+		i++;
+	}
+}
--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,8 +6,26 @@
 /*   By: qcoudeyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:59:41 by qcoudeyr          #+#    #+#             */
-/*   Updated: 2023/02/13 14:35:41 by qcoudeyr         ###   ########.fr       */
+/*   Updated: 2023/02/15 15:53:22 by qcoudeyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned int	i;
+
+	i = 0;
+	str = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
