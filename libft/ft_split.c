2977b489810a75eaf0a60328d45752f5ad2c4845
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-02-15 14:03:27 +0100

    split presque ok

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,23 +6,71 @@
 /*   By: qcoudeyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 12:59:47 by qcoudeyr          #+#    #+#             */
-/*   Updated: 2023/02/14 11:29:57 by qcoudeyr         ###   ########.fr       */
+/*   Updated: 2023/02/15 13:36:58 by qcoudeyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
-char	**ft_split(char const *s, char c)
+static size_t	ft_wordlen(const char *s, char chr)
+{
+	int	c;
+
+	c = 0;
+	while (*s != 0 && *s != chr)
+	{
+		c++;
+		s++;
+	}
+	return (c);
+}
+
+static int	ft_wordct(const char *s, char chr)
+{
+	int	c;
+	int	index;
+
+	c = 0;
+	index = 0;
+	while (s[index] != 0)
+	{
+		while (s[index] == chr && s[index] != 0)
+			index++;
+		if (s[index] != chr && s[index] != 0)
+			c++;
+		while (s[index] != chr && s[index] != 0)
+			index++;
+	}
+	return (c);
+}
+
+char	**ft_split(char const *str, char c)
 {
 	int		i;
-	int		j;
-	char	**str;
+	int		index;
+	char	**tab;
+	char	*s;
 
+	s = ft_strtrim (str, &c);
 	i = 0;
-	j = 0;
-	while (s[i])
+	index = 0;
+	if (!*s)
 	{
-		
+		tab = malloc(sizeof(char *) * 1);
+		tab[0] = malloc(sizeof(char) * 1);
+		tab[0] = 0;
+		return (tab);
 	}
-	return (str);
+	tab = (char **)malloc(sizeof(char *) * ft_wordct(s, c) + 1);
+	while (s[i] || (s[i] == c && s[i+1] == 0))
+	{
+		while (s[i] == c && s[i] != 0)
+			i++;
+		tab[index] = malloc(sizeof(char) * ft_wordlen(&s[i], c) + 1);
+		ft_strlcpy(tab[index++], &s[i], ft_wordlen(&s[i], c) + 1);
+		i += ft_wordlen(&s[i], c);
+	}
+	tab[index] = NULL;
+	return (tab);
 }
