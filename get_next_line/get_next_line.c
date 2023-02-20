5d421334f6b587770aa6d80d80366a79b53f1a8b
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-02-20 15:46:53 +0100

    test et main

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,10 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qcoudeyr <qcoudeyr@student.42perpignan.    +#+  +:+       +#+        */
+/*   By:   <qcoudeyr@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:08:11 by qcoudeyr          #+#    #+#             */
-/*   Updated: 2023/02/17 19:08:17 by qcoudeyr         ###   ########.fr       */
+/*   Updated: 2023/02/20 15:46:07 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+char	*get_next_line(int fd)
+{
+	
+}
+
+
+int	main(void)
+{
+	char	*line;
+	int		i;
+	int		fd1;
+	int		fd2;
+	int		fd3;
+
+	fd1 = open("tests/test1", O_RDONLY);
+	fd2 = open("tests/test2", O_RDONLY);
+	fd3 = open("tests/test3", O_RDONLY);
+	fd4 = open("tests/test4", O_RDONLY);
+	i = 1;
+	while (i < 7)
+	{
+		line = get_next_line(fd1);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		line = get_next_line(fd2);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		line = get_next_line(fd3);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		line = get_next_line(fd4);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(fd1);
+	close(fd2);
+	close(fd3);
+	close(fd4);
+	return (0);
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,16 @@
 /*   By: qcoudeyr <qcoudeyr@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 19:06:54 by qcoudeyr          #+#    #+#             */
-/*   Updated: 2023/02/17 19:08:08 by qcoudeyr         ###   ########.fr       */
+/*   Updated: 2023/02/20 14:55:20 by qcoudeyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
