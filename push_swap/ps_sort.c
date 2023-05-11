cab531c707e66c73c609dd3372e2488fec0747d8
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-05-11 11:17:36 +0200

    fin de l'algo 100%

--- push_swap/ps_sort.c
+++ push_swap/ps_sort.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 09:40:44 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/05/10 12:08:27 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/05/11 11:16:53 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,22 +49,6 @@ int	ft_isrevsort(int *arr, int len)
 	else
 		return (0);
 }
-/*
-static int	pos_vmax(t_ps *t)
-{
-	int	i;
-	int	temp;
-
-	i = 0;
-	temp = 0;
-	while (i < t->len_b)
-	{
-		if (t->pb[temp] < t->pb[i])
-			temp = i;
-		i++;
-	}
-	return (temp);
-} */
 
 void	sort_array(t_ps *t)
 {
@@ -77,83 +61,99 @@ void	sort_array(t_ps *t)
 	push_b(t);
 	if (t->pb[0] < t->pb[1])
 		swap_pile(t, 'b');
-	while (ft_issort(t) != 1 && t->len_a > 1)
+	while (t->len_a > 0)
 	{
-		tab[2] = 100;
+		tab[2] = t->len_a + t->len_b;
 		minmax(t);
 		while (i < t->len_a)
 		{
 			if (t->pa[i] < t->pb[t->bmin])
 				j = t->bmin + 1;
+			else if (t->pa[i] > t->pb[t->bmax])
+				j = t->bmax;
 			else
 			{
 				while (t->pa[i] > t->pb[j] && j < t->len_b)
 					j++;
-				if (t->pa[i] > t->pb[t->bmax])
-					j = t->bmax;
 				while (t->pa[i] < t->pb[j] && j < t->len_b)
 					j++;
 			}
-			sum = i;
-			sum += j;
+			if (i > (t->len_a / 2))
+				sum = t->len_a - i;
+			else
+				sum = i;
+			if (j > t->len_b / 2)
+				sum += t->len_b - j;
+			else
+				sum += j;
+			if (j == t->len_b)
+				j = 0;
 			if (sum < tab[2])
 			{
 				tab[0] = i;
 				tab[1] = j;
 				tab[2] = sum;
 			}
+			j = 0;
 			i++;
 		}
 		i = tab[0];
 		j = tab[1];
-		while (i != 0 && j != 0)
+		while (i != 0 && j != 0 && i < t->len_a / 2 && j < t->len_b / 2)
 		{
 			rotate_pile(t, 'r');
 			i--;
 			j--;
+		}
+		while (i != 0 && j != 0 && i > t->len_a / 2 && j > t->len_b / 2)
+		{
+			rrotate_pile(t, 'r');
+			i++;
+			j++;
+			if (i == t->len_a)
+				i = 0;
+			if (j == t->len_b)
+				j = 0;
+			if (i == 0 && j != 0 && j < t->len_b / 2)
+				j--;
+		}
+
+		while (i != 0 && i > t->len_a / 2 && i < t->len_a)
+		{
+			rrotate_pile(t, 'a');
+			i++;
+			if (i == t->len_a)
+				i = 0;
 		}
 		while (i != 0)
 		{
 			rotate_pile(t, 'a');
 			i--;
 		}
-		while (j != 0 && j != t->len_b)
+		while (j != 0 && j > t->len_b / 2 && j < t->len_b)
+		{
+			rrotate_pile(t, 'b');
+			j++;
+			if (j == t->len_b)
+				j = 0;
+		}
+		while (j != 0)
 		{
 			rotate_pile(t, 'b');
 			j--;
 		}
-		if (j == t->len_b)
-		{
-			push_b(t);
-			rotate_pile(t, 'b');
-			j = 0;
-		}
-		else
+		if (i == 0)
 			push_b(t);
 	}
-}
-/* {
-	int	size;
-
-	size = t->len_a;
-	t->i = 0;
-	while (ft_issort(t) == 0 && t->i < t->len_a)
+	minmax(t);
+	while (t->bmax != 0)
 	{
-		t->j = 1;
-		while (t->j < size + 1)
-		{
-			if (((t->pa[0] >> t->i) & 1) == 1)
-				rotate_pile(t, 'a');
-			else if (ft_issort(t) == 1)
-				break ;
-			else
-				push_b(t);
-			t->j++;
-		}
-		while (t->len_b > 0)
-			push_a(t);
-		t->i++;
+		minmax(t);
+		if (t->bmax > (t->len_b / 2))
+			rrotate_pile(t, 'b');
+		else if (t->bmax < (t->len_b / 2) && t->bmax != 0)
+			rotate_pile(t, 'b');
 	}
+	while (t->len_b > 0)
+		push_a(t);
 }
-
- */
