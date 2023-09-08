ad78b355a4a6e6df1847f496b505f2b4cc786615
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-09-08 12:16:18 +0000

    8 sept. 2023, 14:16

--- fdf/draw.c
+++ fdf/draw.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 10:19:14 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/09/08 14:15:56 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/09/08 14:16:10 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void	line_put(t_mlx *lib, t_map *first, t_map *second)
 	y1 = (first->y * lib->scaley);
 	x2 = (second->x * lib->scalex);
 	y2 = (second->y * lib->scaley);
-	put_dot(&lib->data, (first->y * lib->scaley), (first->x * lib->scalex),tcolor(first->value,0,0));
+	put_dot(&lib->data, x1, y1,tcolor(first->value,0,0));
 	put_dot(&lib->data, (second->y * lib->scaley), (second->x * lib->scalex), tcolor(second->value,0,0));
 
 
