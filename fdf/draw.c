5697ce664f55fbec6b5608193b3f9fe83b07035e
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-09-08 11:59:18 +0000

    8 sept. 2023, 13:59

--- fdf/draw.c
+++ fdf/draw.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 10:19:14 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/09/08 13:59:07 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/09/08 13:59:14 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,7 @@ void	addmap(t_mlx *lib, t_data *data)
 	{
 		while(lib->map != NULL)
 		{
-			line_put(,tcolor((lib->map->value),0,0));
+			line_put(lib,tcolor((lib->map->value),0,0));
 			lib->map = lib->map->nx;
 		}
 		lib->map = line->ny;
