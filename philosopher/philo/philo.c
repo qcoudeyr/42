ff46aed67b08bf1467fe47f45ffbc3a7d132cee8
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-10-02 09:11:12 +0000

    2 oct. 2023, 11:11

--- philosopher/philo/philo.c
+++ philosopher/philo/philo.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:19:03 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/02 11:11:06 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/02 11:11:11 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void	ft_init(t_var *var)
 {
-	var = malloc(sizeof(t_var))
+	var = malloc(sizeof(t_var) *1);
+	
 }
 
 
