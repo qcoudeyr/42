d566da5f91f5ce0b19c99978a9fc99c673b51fad
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-10-02 08:27:41 +0000

    2 oct. 2023, 10:27

--- philosopher/philo/philo.h
+++ philosopher/philo/philo.h
@@ -6,15 +6,15 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:19:05 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/02 10:27:35 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/02 10:27:39 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-#include <stdio.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
