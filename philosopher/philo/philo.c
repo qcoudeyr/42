6983ec5e7c64bc111f225a19363196b20c88608e
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-10-02 09:19:51 +0000

    2 oct. 2023, 11:19

--- philosopher/philo/philo.c
+++ philosopher/philo/philo.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:19:03 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/02 11:19:45 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/02 11:19:49 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_init(t_var *var)
 
 void	ft_readarg(int argv, char **argc, t_var *var)
 {
-	var->n_philo = arg
+	var->n_philo = argv;
 	printf("argv =%i", argv);
 	void **(argc);
 }
