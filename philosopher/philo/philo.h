932427af3a908460d9fd0d1ae7a3c4c92b52cf54
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-10-02 09:13:11 +0000

    2 oct. 2023, 11:13

--- philosopher/philo/philo.h
+++ philosopher/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:19:05 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/02 11:12:18 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/02 11:13:10 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ typedef struct s_philosopher
 
 typedef struct s_var
 {
-	
+	n_philo;
+
 }	t_var;
 
 #endif
