06df9b72d8c88547b233335a3bc9ab6793acc6d3
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-10-02 09:06:00 +0000

    2 oct. 2023, 11:06

--- philosopher/philo/philo.h
+++ philosopher/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:19:05 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/02 11:05:39 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/02 11:05:55 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include <sys/time.h>
 # include <pthread.h>
 
-typedef struct	t_philo
+typedef struct t_philo
 {
 
 }	t_p;
