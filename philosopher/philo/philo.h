1b1d22375bd04ed21c887cc021877bc5a3b34ebf
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-10-02 09:30:00 +0000

    2 oct. 2023, 11:30

--- philosopher/philo/philo.h
+++ philosopher/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:19:05 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/02 11:16:10 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/02 11:29:57 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,32 @@ typedef struct s_var
 
 }	t_var;
 
+// Text colors
+# define COLOR_BLACK   "\033[30m"
+# define COLOR_RED     "\033[31m"
+# define COLOR_GREEN   "\033[32m"
+# define COLOR_YELLOW  "\033[33m"
+# define COLOR_BLUE    "\033[34m"
+# define COLOR_MAGENTA "\033[35m"
+# define COLOR_CYAN    "\033[36m"
+# define COLOR_WHITE   "\033[37m"
+
+// Background colors
+# define BG_BLACK   "\033[40m"
+# define BG_RED     "\033[41m"
+# define BG_GREEN   "\033[42m"
+# define BG_YELLOW  "\033[43m"
+# define BG_BLUE    "\033[44m"
+# define BG_MAGENTA "\033[45m"
+# define BG_CYAN    "\033[46m"
+# define BG_WHITE   "\033[47m"
+
+// Text attributes
+# define RESET     "\033[0m"
+# define BOLD      "\033[1m"
+# define DIM       "\033[2m"
+# define UNDERLINE "\033[4m"
+# define BLINK     "\033[5m"
+# define REVERSE   "\033[7m"
+# define HIDDEN    "\033[8m"
 #endif
