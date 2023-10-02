0391a0a5d8eec9ce53c00ede06a451906da0999c
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-10-02 09:32:21 +0000

    2 oct. 2023, 11:32

--- philosopher/philo/utils.c
+++ philosopher/philo/utils.c
@@ -6,9 +6,37 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 11:30:45 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/02 11:30:55 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/02 11:32:16 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static int	ft_isspace(const char c)
+{
+	if (c == 32 || (c >= 9 && c <= 13))
+		return (1);
+	else
+		return (0);
+}
+
+long int	ft_atoi(const char *str)
+{
+	long long int	value;
+	int				neg;
+
+	value = 0;
+	neg = 1;
+	while (ft_isspace(*(char *)str) == 1)
+		str++;
+	if (*(char *)str == '-')
+	{
+		neg *= -1;
+		str++;
+	}
+	else if (*(char *)str == '+')
+		str++;
+	while (*(unsigned char *)str < 58 && *(unsigned char *)str > 47)
+		value = (value * 10 + (*str++ - 48));
+	return (value * neg);
+}
