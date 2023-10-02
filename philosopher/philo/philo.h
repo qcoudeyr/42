0a1d72cb5647b61c24462e8480284a13d9411f77
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-10-02 08:27:56 +0000

    2 oct. 2023, 10:27

--- philosopher/philo/philo.h
+++ philosopher/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:19:05 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/02 10:27:47 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/02 10:27:53 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include <sys/types.h>
 # include <signal.h>
 # include <sys/time.h>
-# include <pthread.h>    // Pour pthread_create, pthread_detach, pthread_join
+# include <pthread.h>
 # include <sys/wait.h>   // Pour waitpid
 # include <semaphore.h>
 
