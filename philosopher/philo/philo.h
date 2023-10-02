bb333f3f610f9e34c16e5db9ac788e67e09953d3
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-10-02 08:27:30 +0000

    2 oct. 2023, 10:27

--- philosopher/philo/philo.h
+++ philosopher/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:19:05 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/02 10:27:23 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/02 10:27:30 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>     // Pour write, fork, usleep
+#include <unistd.h>
 #include <sys/types.h>  // Pour getpid, fork, kill, waitpid
 #include <signal.h>     // Pour kill
 #include <sys/time.h>   // Pour gettimeofday
