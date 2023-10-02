b7ff952cdeeda5081da8179c2966b690b420784d
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-10-02 08:22:25 +0000

    2 oct. 2023, 10:22

--- philosopher/philo.c
+++ philosopher/philo.c
@@ -6,10 +6,13 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:19:03 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/02 10:22:19 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/02 10:22:24 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 int	main(int argc, char **argv)
+{
+
+}
