7ca5f1d00c24524ec62581c925acf572d319a496
qcoudeyr <qcoudeyr@student.42perpignan.fr>
2023-10-02 09:33:58 +0000

    2 oct. 2023, 11:33

--- philosopher/philo/philo.h
+++ philosopher/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 10:19:05 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/02 11:33:49 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/02 11:33:57 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,12 @@
 
 typedef struct s_philosopher
 {
-
 }	t_philo;
 
 typedef struct s_var
 {
 	int		n_philo;
 	int		tt[4];
-
 }	t_var;
 
 long int	ft_atoi(const char *str);
