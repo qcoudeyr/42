909bb0f610e201a80e0bc2eb91c208472f59a125
Qcoudeyr <qcoudeyr@pm.me>
2023-10-07 12:04:05 +0000

    Oct 7, 2023, 2:04 PM

--- philosopher/philo/routine.c
+++ philosopher/philo/routine.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 11:53:31 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/07 13:54:46 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/07 14:02:03 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	ft_start_routine(t_var *var)
 {
-	
+	pthread_mutex_t	mutex;
+
+	pthread_mutex_init(&mutex, NULL);
 }
