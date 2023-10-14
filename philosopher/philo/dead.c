2ee8cca9dd478e023e5a622219132b1cab1aa8ae
Qcoudeyr <qcoudeyr@pm.me>
2023-10-14 12:20:50 +0000

    Oct 14, 2023, 2:20 PM

--- philosopher/philo/dead.c
+++ philosopher/philo/dead.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:04:44 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/14 14:05:19 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/14 14:18:08 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,9 @@ int	ft_eat_dead(t_philo *p)
 	long			time;
 
 	pthread_mutex_lock(p->time_lock);
-
 	if (((p->tt[1] + p->n_philo->last_eat) >= p->last_eat + p->tt[0]))
 	{
-		time = (p->tt[1] + p->n_philo->last_eat) - (p->last_eat + p->tt[0]);
+		time = (p->last_eat + p->tt[0]) - *p->start_time;
 		pthread_mutex_unlock(p->time_lock);
 		usleep(time * 1000);
 		pthread_mutex_lock(p->end_lock);
@@ -42,9 +41,8 @@ int	ft_eat_dead(t_philo *p)
 		p->alive = 0;
 		gettimeofday(&end, NULL);
 		pthread_mutex_lock(p->time_lock);
-		if (*p->start_time > 0)
-			m_printf(COLOR_RED"%li ms: %i died eat_dead\n", ((p->last_eat + p->tt[0]) \
-			- *p->start_time), p);
+		if (*p->start_time > 0 && pthread_mutex_unlock(p->time_lock))
+			m_printf(COLOR_RED"%li ms: %i died eat_dead\n", time, p);
 		*p->start_time = -1;
 		pthread_mutex_unlock(p->time_lock);
 		return (-1);
@@ -61,19 +59,19 @@ int	ft_dead(t_philo *p)
 	dead_check(p);
 	gettimeofday(&end, NULL);
 	pthread_mutex_lock(p->time_lock);
-	elapsed_ms = ((((end.tv_sec % 1000) * 1000) + (end.tv_usec / 1000)) \
-	- p->last_eat);
+	elapsed_ms = ((((end.tv_sec % 1000) * 1000) + (end.tv_usec / 1000));
 	pthread_mutex_unlock(p->time_lock);
-	if (elapsed_ms >= p->tt[0] || (p->n_philo == NULL && p->num == 1))
+	if (elapsed_ms - p->last_eat >= p->tt[0] || (p->n_philo == 0 && p->num == 1))
 	{
 		pthread_mutex_lock(p->end_lock);
 		*p->end += 1;
 		p->alive = 0;
 		pthread_mutex_unlock(p->end_lock);
 		pthread_mutex_lock(p->time_lock);
-		if (*p->start_time > 0)
-			m_printf(COLOR_RED"%li ms: %i died ft_dead\n", ((((end.tv_sec % 1000) * 1000) \
-			+ (end.tv_usec / 1000)) - *p->start_time), p);
+		elapsed_ms -= *p->start_time;
+		if (*p->start_time > 0 && pthread_mutex_unlock(p->time_lock))
+			m_printf(COLOR_RED"%li ms: %i died ft_dead\n", elapsed_ms, p);
+		pthread_mutex_lock(p->time_lock);
 		*p->start_time = -1;
 		pthread_mutex_unlock(p->time_lock);
 		return (-1);
