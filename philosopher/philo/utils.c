be0f913b7bb5b33e2a90dd91fb76e6cd8bda18d1
Qcoudeyr <qcoudeyr@pm.me>
2023-10-16 10:14:28 +0000

    Oct 16, 2023, 12:14 PM

--- philosopher/philo/utils.c
+++ philosopher/philo/utils.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 11:30:45 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/10/16 12:07:39 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/10/16 12:10:19 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,19 @@ int	ft_usleep(t_philo *p, long sleep)
 {
 	long			total;
 	long			time;
+	struct timeval	end;
 
 	if (dead_check(p) == -1)
 		return (-1);
+	gettimeofday(&end, NULL);
 	time = ft_time(p);
 	pthread_mutex_lock(p->time_lock);
-	printf("time = %li last_eat=%li", time, p->last_eat);
-	time -= (p->last_eat / 1000);
+	time -= (((end.tv_sec % 1000) * 1000) + (end.tv_usec / 1000)) - p->last_eat;
 	total = time + (sleep / 1000);
 	if (total >= p->tt[0])
 	{
 		total = ((p->last_eat + p->tt[0]) - time);
+		printf("total =%li", total);
 		pthread_mutex_unlock(p->time_lock);
 		usleep(time * 1000);
 		execute_dead(p, ft_time(p));
