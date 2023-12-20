816018a3c519de89a45045e76156b39aa2c478af
Qcoudeyr <qcoudeyr@pm.me>
2023-12-20 10:00:59 +0000

    Dec 20, 2023, 11:00 AM

--- CPP/cpp00/ex01/PhoneBook.cpp
+++ CPP/cpp00/ex01/PhoneBook.cpp
@@ -1,30 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/20 10:58:41 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/20 10:58:42 by  qcoudeyr        ###   ########.fr       */
+/*   Created: 2023/12/20 10:58:34 by  qcoudeyr         #+#    #+#             */
+/*   Updated: 2023/12/20 11:00:17 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char **argv)
-{
-	std::string outputstr;
-
-	if (argc < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i = 1; i < argc; i++)
-	{
-		for (int x = 0; argv[i][x] != 0; x++)
-			std::cout << (char)std::toupper(argv[i][x]);
-		std::cout << " ";
-
-	}
-	std::cout << std::endl;
-	return 0;
-}
+int	main()
