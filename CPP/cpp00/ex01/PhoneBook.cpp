99d86d90aab9a5782f15b938f534663a4cb85561
Qcoudeyr <qcoudeyr@pm.me>
2023-12-20 10:26:26 +0000

    Dec 20, 2023, 11:26 AM

--- CPP/cpp00/ex01/PhoneBook.cpp
+++ CPP/cpp00/ex01/PhoneBook.cpp
@@ -6,12 +6,23 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 10:58:34 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/20 11:14:26 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/20 11:26:26 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string.h>
+
+void MenuHeader(void)
+{
+	std::cout<<"#######################"<<std::endl;
+	std::cout<<"#       Welcome       #"<<std::endl;
+	std::cout<<"#         to          #"<<std::endl;
+	std::cout<<"#        your         #"<<std::endl;
+	std::cout<<"#      PhoneBook      #"<<std::endl;
+	std::cout<<"#######################"<<std::endl;
+	std::cout<<"\nWhat do you want to do ?"<<std::endl;
+}
 
 int	main(int argc, char **argv)
 {
@@ -24,6 +35,8 @@ int	main(int argc, char **argv)
 	}
 	while (1)
 	{
+		MenuHeader();
+		std::cout<<"$> ";
 		std::cin>>input;
 		if (input == "EXIT")
 			break;
@@ -34,6 +47,10 @@ int	main(int argc, char **argv)
 		else if (input == "SEARCH")
 		{
 			//Do search thing
+		}
+		else
+		{
+			std::cout<<"\n Nothing can be done with" + input
 		}
 
 	}
