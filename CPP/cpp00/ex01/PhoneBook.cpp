d95f16c35ed16bff5e47dee810e8070db06ee0dc
Qcoudeyr <qcoudeyr@pm.me>
2023-12-20 10:04:37 +0000

    Dec 20, 2023, 11:04 AM

--- CPP/cpp00/ex01/PhoneBook.cpp
+++ CPP/cpp00/ex01/PhoneBook.cpp
@@ -6,10 +6,16 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 10:58:34 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/20 11:00:17 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/20 11:04:10 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main()
+int	main(int argc, char **argv)
+{
+	(void) argv;
+	if (argc != 1)
+		return (0 && std::cout<<"Error ! Usage : $>./PhoneBook\n");
+
+}
