34ad1c58c5c832da73d9dfe4d8eabafb26e49e06
Qcoudeyr <qcoudeyr@pm.me>
2023-12-24 18:58:41 +0000

    Dec 24, 2023, 7:58 PM

--- cub3d/main/main.c
+++ cub3d/main/main.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 18:59:42 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/24 19:55:39 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/24 19:58:37 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 int	check_arg(int argc, char **argv)
 {
+	char *temp;
+
 	if (argc > 2)
 		return (-1);
 	else if (argc == 1)
 	{
 		ft_printf("No map entered, do you want to load the default one ? \nY/N $>");
+		temp = get_next_line(0);
+		if (temp && *temp &&ft_strrchr("yY", *temp) == 0)
+			printf(CL_RED BOLD"Error, no map entered !"RESET);
+		}
 
 	}
 }
