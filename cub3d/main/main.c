76660c9c02b983c3a888ec3307ad39ea2d561db8
Qcoudeyr <qcoudeyr@pm.me>
2023-12-24 18:55:40 +0000

    Dec 24, 2023, 7:55 PM

--- cub3d/main/main.c
+++ cub3d/main/main.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 18:59:42 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/24 19:49:28 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/24 19:55:39 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ int	check_arg(int argc, char **argv)
 	if (argc > 2)
 		return (-1);
 	else if (argc == 1)
-	// ask to load the default map;
+	{
+		ft_printf("No map entered, do you want to load the default one ? \nY/N $>");
+
+	}
 }
 
 int	main(int argc, char **argv)
