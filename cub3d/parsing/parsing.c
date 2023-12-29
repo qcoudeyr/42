253c3274dbac8578771173ed59983ac0a47e3f5c
Qcoudeyr <qcoudeyr@pm.me>
2023-12-29 13:23:27 +0000

    Dec 29, 2023, 2:23 PM

--- cub3d/parsing/parsing.c
+++ cub3d/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 13:19:45 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/29 14:20:00 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2023/12/29 14:23:26 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,7 @@ int	parse(char *str, t_mlx *lib, int x, t_map *p_x)
 	return (1);
 }
 
-char	*get_texture_path(t_cub *t, char *str)
+char	*get_texture_path(char *str)
 {
 	char	*temp;
 	int		i;
@@ -66,7 +66,7 @@ char	*get_texture_path(t_cub *t, char *str)
 	i = 0;
 	temp = ft_calloc(1000, sizeof(char));
 	while (str[i] && str[i] != '\n')
-		temp[i] = str[i++];
+		temp[i++] = str[i];
 	temp[i] = 0;
 	return (temp);
 }
@@ -82,22 +82,31 @@ void	get_map_info(t_cub *t, int fd)
 	s = 0;
 	temp = ft_strnstr(buf, "NO ", 3);
 	if (temp != NULL && *temp != 0 && ft_strnstr(temp +2, "NO ", 3) == NULL)
-		get_texture_path(t, temp + 3);
+		t->lib->fd_no = open("get_texture_path(temp + 3)", O_RDONLY);
 	else
 		return ;
 	temp = ft_strnstr(buf, "SO ", 3);
 	if (temp != NULL && *temp != 0 && ft_strnstr(temp +2, "SO ", 3) == NULL)
-		get_texture_path(t, temp + 3);
+		t->lib->fd_so = open("get_texture_path(temp + 3)", O_RDONLY);
 	else
 		return ;
 	temp = ft_strnstr(buf, "WE ", 3);
 	if (temp != NULL && *temp != 0 && ft_strnstr(temp +2, "WE ", 3) == NULL)
-		get_texture_path(t, temp + 3);
+		t->lib->fd_we = open("get_texture_path(temp + 3)", O_RDONLY);
 	else
 		return ;
 	temp = ft_strnstr(buf, "EA ", 3);
 	if (temp != NULL && *temp != 0 && ft_strnstr(temp +2, "EA ", 3) == NULL)
-		get_texture_path(t, temp + 3);
+		t->lib->fd_ea = open("get_texture_path(temp + 3)", O_RDONLY);
+	else
+		return ;
+	temp = ft_strnstr(buf, "F ", 3);
+	if (temp != NULL && *temp != 0 && ft_strnstr(temp +2, "F ", 2) == NULL)
+		t->lib->fd_ea = open("get_texture_path(temp + 3)", O_RDONLY);
+	else
+		return ;
+	if (temp != NULL && *temp != 0 && ft_strnstr(temp +2, "C ", 2) == NULL)
+		t->lib->fd_ea = open("get_texture_path(temp + 3)", O_RDONLY);
 	else
 		return ;
 }
