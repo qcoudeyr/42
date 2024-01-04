993bd69961f9cc036ceb85c8dc076a69f8dcef52
Qcoudeyr <qcoudeyr@pm.me>
2024-01-04 09:49:14 +0000

    Jan 4, 2024, 10:49 AM

--- cub3d/rendering/rendering.c
+++ cub3d/rendering/rendering.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 16:34:42 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2024/01/04 10:37:59 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2024/01/04 10:47:36 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 void	sqr_print(t_data *data, int lenx, int leny, int color)
 {
-	t_utils	*u;
+	t_utils *u;
 
 	u = utils_init;
+	while (u->x < lenx)
 }
