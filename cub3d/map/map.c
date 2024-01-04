a89b4d39729e6ea9d8c51fd76d9d9aa5ed182b83
Qcoudeyr <qcoudeyr@pm.me>
2024-01-04 08:45:32 +0000

    Jan 4, 2024, 9:45 AM

--- cub3d/map/map.c
+++ cub3d/map/map.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 16:33:30 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2024/01/04 09:41:55 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2024/01/04 09:45:08 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ int	check_error_map(t_map *map)
 {
 	if (map == NULL)
 		return (-1);
-	if (!map->px || !map->py == NULL || map->nx == NULL || map->ny == NULL)
+	if ((!map->px || !map->py || !map->nx || !map->ny) && map->value != 1 || map->value != -1)
+		return (-1);
+	else if (map->px->value == -1 || map->py->value == -1)
 }
 
 void	addmap(t_mlx *lib, t_data *data)
