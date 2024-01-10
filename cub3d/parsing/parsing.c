b9eb6e27df64e941aacd51b61afed4c9a17296b0
Qcoudeyr <qcoudeyr@pm.me>
2024-01-10 15:31:17 +0000

    Jan 10, 2024, 4:31 PM

--- cub3d/parsing/parsing.c
+++ cub3d/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 13:19:45 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2024/01/10 16:14:59 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2024/01/10 16:27:05 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -256,10 +256,10 @@ if (check_map(fd) == -1)
 
 int	get_texture(t_mlx *lib)
 {
-	lib->no.ptr = mlx_png_file_to_image(lib->mlx, lib->no.fname, &lib->no.w, &lib->no.h);
-	lib->so.ptr = mlx_png_file_to_image(lib->mlx, lib->so.fname, &lib->so.w, &lib->so.h);
-	lib->ea.ptr = mlx_png_file_to_image(lib->mlx, lib->ea.fname, &lib->ea.w, &lib->ea.h);
-	lib->we.ptr = mlx_png_file_to_image(lib->mlx, lib->we.fname, &lib->we.w, &lib->we.h);
+	lib->no.ptr = mlx_xpm_file_to_image(lib->mlx, "/home/qcoudeyr/Desktop/42/cub3d/map/64x/Bricks.xpm", &lib->no.w, &lib->no.h);
+	lib->so.ptr = mlx_xpm_file_to_image(lib->mlx, lib->so.fname, &lib->so.w, &lib->so.h);
+	lib->ea.ptr = mlx_xpm_file_to_image(lib->mlx, lib->ea.fname, &lib->ea.w, &lib->ea.h);
+	lib->we.ptr = mlx_xpm_file_to_image(lib->mlx, lib->we.fname, &lib->we.w, &lib->we.h);
 	if (!lib->no.ptr || !lib->so.ptr || !lib->ea.ptr || !lib->we.ptr)
 		return (printerr("Error !\nMap textures are not valid !\n"));
 	else
