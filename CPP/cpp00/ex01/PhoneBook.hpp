e810fe24806d4e96686bf4c9c1536edbdcd82632
Qcoudeyr <qcoudeyr@pm.me>
2024-01-22 10:42:50 +0000

    Jan 22, 2024, 11:42 AM

--- CPP/cpp00/ex01/PhoneBook.hpp
+++ CPP/cpp00/ex01/PhoneBook.hpp
@@ -6,19 +6,14 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 10:58:36 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2023/12/20 11:46:41 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2024/01/22 11:40:08 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 class PhoneBook {
-	private:
-		std::string FirstName;
-		std::string LastName;
-		std::string NickName;
-		std::string PhoneNumber;
-		std::string DarkSecret;
+	
 };
 
 class Contact {
