41ddb70a872cacfd18a2e8978e6208a52af62288
Qcoudeyr <qcoudeyr@pm.me>
2024-01-24 10:36:36 +0000

    Jan 24, 2024, 11:36 AM

--- CPP/cpp01/ex04/main.cpp
+++ CPP/cpp01/ex04/main.cpp
@@ -6,15 +6,40 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 18:56:04 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2024/01/23 19:38:12 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2024/01/24 11:34:29 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include ".hpp"
+#include <string>
+#include <fstream>
 
 int main()
 {
+	const char		*filePath = "origin.file";
+	std::string		s1 = "ipsum";
+	std::string		s2 = "TEST";
+	std::string		line;
 
+	std::ifstream inputFile(filePath);
+	if (!inputFile.is_open())
+		return (std::cerr << "Error opening the file: " << filePath << std::endl, 1);
+
+	std::ofstream outputFile(filePath + ".replace");
+	if (!outputFile.is_open())
+		return (std::cerr << "Error creating the file: " << filePath << std::endl, 1);
+	while (std::getline(inputFile, line))
+	{
+		size_t found = s1.find(s2);
+		if (found != std::string::npos)
+		{
+			std::cout << "Substring found at position " << found << std::endl;
+		}
+		else
+			outputFile << line;
+	}
+
+	inputFile.close();
+	outputFile.close();
 	return (0);
 }
