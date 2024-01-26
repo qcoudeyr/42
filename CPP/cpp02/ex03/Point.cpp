549b4f485a265ff03c7da6f8fedade2cf97bb3fa
Qcoudeyr <qcoudeyr@pm.me>
2024-01-26 14:00:33 +0000

    Jan 26, 2024, 3:00 PM

--- CPP/cpp02/ex03/Point.cpp
+++ CPP/cpp02/ex03/Point.cpp
@@ -6,13 +6,13 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 11:31:53 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2024/01/26 14:28:36 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2024/01/26 14:54:42 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-Point::Point(Fixed x, Fixed y): _x(x), _y(y)
+Point::Point(Fixed x, Fixed y): _x(0), _y(0)
 {
 }
 
@@ -29,6 +29,11 @@ Point& Point::operator=(const Point& other)
 	if (this == &other)
 		return *this;
 	return *this;
+}
+
+void Point::setValue(float xValue, float yValue)
+{
+	this->_x.setRawBits(value);
 }
 
 Fixed Point::getX() const
