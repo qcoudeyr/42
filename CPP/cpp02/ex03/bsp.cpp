d3536e284c9859e3e32f4440d2b1f268f2973665
Qcoudeyr <qcoudeyr@pm.me>
2024-01-26 13:24:03 +0000

    Jan 26, 2024, 2:24 PM

--- CPP/cpp02/ex03/bsp.cpp
+++ CPP/cpp02/ex03/bsp.cpp
@@ -6,13 +6,28 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 11:31:51 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2024/01/26 13:28:56 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2024/01/26 14:22:02 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
+Fixed sign(Point p1, Point p2, Point p3)
+{
+	return (p1.getX.toFloat() - p3.getX()) * (p2.getY() - p3.getY()) - (p2.getX() - p3.getX()) * (p1.getY() - p3.getY());
+}
+
 bool	bsp( Point const a, Point const b, Point const c, Point const point)
 {
+	Fixed d1, d2, d3;
+	bool has_neg, has_pos;
 
+	d1 = sign(point, a, b);
+	d2 = sign(point, b, c);
+	d3 = sign(point, c, a);
+
+	has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+	has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+	return !(has_neg && has_pos);
 }
--- CPP/cpp02/ex03/main.cpp
+++ CPP/cpp02/ex03/main.cpp
@@ -6,15 +6,27 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 22:18:42 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2024/01/26 13:24:31 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2024/01/26 14:19:39 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Fixed.hpp"
+#include "Point.hpp"
 #include <iostream>
 
-int main( void )
+int main()
 {
+	Point a(25.0f, 25.0f);
+	Point b(42.0f, 42.0f);
+	Point c(30.0f, 2.0f);
+
+	Point testPoint(0.5f, 0.5f);
+
+	bool isInside = bsp(a, b, c, testPoint);
+
+	if (isInside)
+		std::cout << "The point is inside the triangle." << std::endl;
+	else
+		std::cout << "The point is not inside the triangle." << std::endl;
 
 	return 0;
 }
