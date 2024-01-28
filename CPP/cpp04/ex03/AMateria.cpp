ceb2448e94bd98d6507cf2702112db3d699a63bd
Qcoudeyr <qcoudeyr@pm.me>
2024-01-28 13:11:13 +0000

    Jan 28, 2024, 2:11 PM

--- CPP/cpp04/ex03/AMateria.cpp
+++ CPP/cpp04/ex03/AMateria.cpp
@@ -6,7 +6,26 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 14:03:13 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2024/01/28 14:03:14 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2024/01/28 14:08:36 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "AMateria.hpp"
+
+AMateria::AMateria()
+{
+}
+AMateria::~AMateria()
+{
+}
+
+AMateria::AMateria(const AMateria& rhs){
+
+}
+
+
+AMateria& AMateria::operator=(const AMateria& rhs) {
+	if (this != &rhs){
+	}
+	return *this;
+}
--- CPP/cpp04/ex03/AMateria.hpp
+++ CPP/cpp04/ex03/AMateria.hpp
@@ -6,7 +6,23 @@
 /*   By:  qcoudeyr <@student.42perpignan.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 14:03:12 by  qcoudeyr         #+#    #+#             */
-/*   Updated: 2024/01/28 14:03:13 by  qcoudeyr        ###   ########.fr       */
+/*   Updated: 2024/01/28 14:07:03 by  qcoudeyr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+class AMateria
+{
+private:
+
+public:
+	AMateria();
+	~AMateria();
+	AMateria(const AMateria& rhs);
+	AMateria& operator=(const AMateria& rhs);
+
+};
+
+#endif // AMateria_HPP
