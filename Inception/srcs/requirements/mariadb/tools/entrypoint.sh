cd33d51c29f04ab416af3e064e809dd2a2a31343
Qcoudeyr <qcoudeyr@pm.me>
2024-02-15 18:55:20 +0000

    Feb 15, 2024, 7:55 PM

---
+++ Inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -e
+# MariaDB configuration script for WordPress
+
+mysql_install_db --user=$SQL_USER --ldata=$SQL_DATABASE
+
+# Wait for MariaDB to start
+until mysqladmin ping -h localhost --silent; do
+    echo 'Waiting for MariaDB to start...'
+    sleep 1
+done
+echo 'MariaDB started'
+
+# Secure the MariaDB installation (similar to 'mysql_secure_installation')
+mysql -u root -p"$SQL_ROOT_PASSWORD" -h "$SQL_HOST" <<-EOSQL
+    SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$SQL_ROOT_PASSWORD');
+    DELETE FROM mysql.user WHERE User='';
+    DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+    DROP DATABASE IF EXISTS test;
+    DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+    FLUSH PRIVILEGES;
+EOSQL
+
+# Create WordPress database
+mysql -u root -p"$SQL_ROOT_PASSWORD" -h "$SQL_HOST" <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\` CHARACTER SET $SQL_CHARSET COLLATE $SQL_COLLATION;
+EOSQL
+
+# Create WordPress user
+mysql -u root -p"$SQL_ROOT_PASSWORD" -h "$SQL_HOST" <<-EOSQL
+    CREATE USER '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
+    GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO '$SQL_USER'@'%';
+    FLUSH PRIVILEGES;
+EOSQL
+
+# Create an additional user with a username not containing 'admin' or 'administrator'
+# Replace 'nonadminuser' and 'nonadminpassword' with the desired username and password
+mysql -u root -p"$SQL_ROOT_PASSWORD" -h "$SQL_HOST" <<-EOSQL
+    CREATE USER '$SQL_ADDITIONAL_USER'@'%' IDENTIFIED BY '$SQL_ADDITIONAL_PASSWORD';
+    GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO '$SQL_ADDITIONAL_USER'@'%';
+    FLUSH PRIVILEGES;
+EOSQL
+
+echo 'MariaDB configuration completed'
+
+# Hand off to the original entrypoint (to ensure that the original behavior of the image is preserved)
+exec "$@"
