4893ac22a6edd60f648d1042f7e595e3f0d65472
Qcoudeyr <qcoudeyr@pm.me>
2024-02-19 11:28:33 +0000

    Feb 19, 2024, 12:28 PM

--- Inception/srcs/requirements/bonus/redis/tools/redis_init.sh
+++ Inception/srcs/requirements/bonus/redis/tools/redis_init.sh
@@ -8,5 +8,20 @@ echo 'Set the redis pass using the env var'
 
 redis-cli config set requirepass '$REDIS_PASS'
 
+# Find the PID of the redis-server process
+REDIS_PID=$(pgrep redis-server)
+
+# Check if the redis-server process is running
+if [ -z "$REDIS_PID" ]; then
+    echo "Redis server is not running."
+else
+    echo "Redis server is running with PID: $REDIS_PID"
+
+    # Kill the redis-server process
+    kill $REDIS_PID
+    echo "Redis server with PID $REDIS_PID has been killed."
+fi
+
+
 # Hand over control to the main CMD
 exec "$@"
