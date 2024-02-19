cf9d987e2570ff7da5d6a4e14b1c0e94e0703236
Qcoudeyr <qcoudeyr@pm.me>
2024-02-19 11:43:23 +0000

    Feb 19, 2024, 12:43 PM

--- Inception/srcs/requirements/bonus/redis/tools/redis_init.sh
+++ Inception/srcs/requirements/bonus/redis/tools/redis_init.sh
@@ -3,7 +3,25 @@ set -e
 
 echo 'Set the redis pass using the env var'
 
-echo 'requirepass $REDIS_PASS' >> /etc/redis/redis.conf
+check_and_create_folder() {
+    if [ -d "$1" ]; then
+        echo "Folder '$1' already exists."
+    else
+        mkdir -p "$1"
+        echo "Folder '$1' created successfully."
+    fi
+}
+
+check_and_create_folder "/etc/redis/"
+
+# Set the redis password using the environment variable REDIS_PASS
+if [ -n "$REDIS_PASS" ]; then
+    echo "requirepass $REDIS_PASS" >> /etc/redis/redis.conf
+    echo "Redis password set successfully in /etc/redis/redis.conf."
+else
+    echo "Error: REDIS_PASS environment variable is not set."
+    exit 1
+fi
 
 
 # Hand over control to the main CMD
