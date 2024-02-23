856d1cb427590be23a5da9eb014b9efaaf4ab397
Qcoudeyr <qcoudeyr@pm.me>
2024-02-23 12:12:16 +0000

    Feb 23, 2024, 1:12 PM

--- Inception/srcs/requirements/bonus/nodejs/tools/node_website/app.js
+++ Inception/srcs/requirements/bonus/nodejs/tools/node_website/app.js
@@ -1,30 +1,19 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-  console.log(`${req.method} ${req.url}`);
-
-  // Handle CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  // Redirect all requests to views/index.html
-  fs.readFile(__dirname + "/views/index.html", (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      return res.end("Error loading index.html");
-    }
-
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-});
+const app = express();
 
 // Constants
-const PORT = 8080; // Change this to the desired port
-const HOST = "0.0.0.0";
+const PORT = 8080;
 
-server.listen(PORT, HOST, () => {
-  console.log(`Node.js app listening on http://${HOST}:${PORT}`);
+// Serve static files from the "views" directory
+app.use(express.static(path.join(__dirname, "views")));
+
+// Redirect all requests to views/index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Node.js app listening on http://localhost:${PORT}`);
 });
