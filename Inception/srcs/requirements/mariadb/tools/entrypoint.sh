f8351e0adec580226bda239efa1a0715c7f67ef9
Qcoudeyr <qcoudeyr@pm.me>
2024-05-13 09:54:59 +0000

    May 13, 2024, 11:54 AM

--- Inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ Inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -12,66 +12,59 @@ until mysqladmin ping --silent; do
 done
 echo 'MariaDB is available'
 
-# Check if MariaDB has already been configured
-if [ ! -f /var/lib/mysql/configured ]; then
-
-	echo '$SQL_ROOT_PASSWORD'
-	echo 'Set SQL_PASSWORD for mysql server ROOT user'
-	mysql <<-EOSQL
-	SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$SQL_ROOT_PASSWORD');
-	FLUSH PRIVILEGES;
-	EOSQL
+echo '$SQL_ROOT_PASSWORD'
+echo 'Set SQL_PASSWORD for mysql server ROOT user'
+mysql <<-EOSQL
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$SQL_ROOT_PASSWORD');
+FLUSH PRIVILEGES;
+EOSQL
 
 
-	echo 'Adding admin user and normal user'
+echo 'Adding admin user and normal user'
 
-	mysql <<-EOSQL
-	CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
-	GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%' WITH GRANT OPTION;
-	FLUSH PRIVILEGES;
-	EOSQL
+mysql <<-EOSQL
+CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
+GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOSQL
 
 
-	echo 'Create the user and database'
-	mysql  <<-EOSQL
-	CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;
-	CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
-	GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO '$SQL_USER'@'%';
-	FLUSH PRIVILEGES;
-	EOSQL
+echo 'Create the user and database'
+mysql  <<-EOSQL
+CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;
+CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';
+GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO '$SQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOSQL
 
-	echo 'Create the additional user with specific privileges'
-	mysql <<-EOSQL
-	CREATE USER IF NOT EXISTS '$SQL_ADDITIONAL_USER'@'%' IDENTIFIED BY '$SQL_ADDITIONAL_PASSWORD';
-	-- Here you should replace '$SQL_DATABASE' with the actual database name
-	GRANT SELECT, INSERT, UPDATE, DELETE ON \`$SQL_DATABASE\`.* TO '$SQL_ADDITIONAL_USER'@'%';
-	FLUSH PRIVILEGES;
-	EOSQL
+echo 'Create the additional user with specific privileges'
+mysql <<-EOSQL
+CREATE USER IF NOT EXISTS '$SQL_ADDITIONAL_USER'@'%' IDENTIFIED BY '$SQL_ADDITIONAL_PASSWORD';
+-- Here you should replace '$SQL_DATABASE' with the actual database name
+GRANT SELECT, INSERT, UPDATE, DELETE ON \`$SQL_DATABASE\`.* TO '$SQL_ADDITIONAL_USER'@'%';
+FLUSH PRIVILEGES;
+EOSQL
 
-	mysql <<-EOF
-	-- Set the root password
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
-	-- Remove anonymous users
-	DELETE FROM mysql.user WHERE User='';
-	-- Disallow root login remotely
-	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-	-- Remove test database and access to it
-	DROP DATABASE IF EXISTS test;
-	DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-	-- Reload privilege tables
-	FLUSH PRIVILEGES;
-	EOF
+mysql <<-EOF
+-- Set the root password
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
+-- Remove anonymous users
+DELETE FROM mysql.user WHERE User='';
+-- Disallow root login remotely
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+-- Remove test database and access to it
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+-- Reload privilege tables
+FLUSH PRIVILEGES;
+EOF
 
-	echo 'MySQL setup completed'
+echo 'MySQL setup completed'
 
-	touch /var/lib/mysql/configured
+touch /var/lib/mysql/configured
 
-	mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-	wait "$pid"
+wait "$pid"
 
-else
-	echo 'MariaDB has already been configured. Starting the server...'
-fi
-# Hand over control to the main CMD
 exec "$@"
